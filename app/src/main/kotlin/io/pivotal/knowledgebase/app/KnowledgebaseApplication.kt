45420e85aa1c7b94d0148387c437841f693b5169
Alex Basson <alexbasson@gmail.com>
2019-03-16 11:10:30 -0400

    Move application into app module

--- app/src/main/kotlin/io/pivotal/knowledgebase/app/KnowledgebaseApplication.kt
+++ app/src/main/kotlin/io/pivotal/knowledgebase/app/KnowledgebaseApplication.kt
@@ -1,4 +1,4 @@
-package io.pivotal.knowledgebase
+package io.pivotal.knowledgebase.app
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
